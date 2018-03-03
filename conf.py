031308111ac277f23b963634ba68ee4701acdfca
thestrawh8 <thestrawh8@gmail.com>
2018-03-03 19:26:50 +0530

    removing dabba

--- conf.py
+++ conf.py
@@ -136,7 +136,7 @@ NAVIGATION_LINKS = {
     	("/","Home"),
     	("/stories/academic.html","Academic"),
         ((("/archive.html", "Archives"),("/categories/index.html", "Tags")),"Blog"),
-        ("/stories/dabba.html","Dabba"),
+        # ("/stories/dabba.html","Dabba"),
         ("/stories/quotes.html","Progress"),
         ("/stories/contact.html", "Contact")
     ),
